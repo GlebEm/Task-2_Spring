de6ba3c04965bc6d4ea2b92a6510d480ee2c6bc6
GlebEm <132710155+GlebEm@users.noreply.github.com>
2023-07-10 13:09:44 +0300

    Update Cat.java

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -2,10 +2,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-////В классе AppConfig, по аналогии, создайте бин с именем “cat”.
-//Настройте этот бин так, чтобы он создавался новым при каждом запросе.
-// нужно, чтобы бин создавался не один раз во время старта приложения,
-// а каждый раз во время вызова бина. Для этого нужно указать Scope для бина, в вашем случае - это prototype
+
 @Configuration
 public class Cat {
     private String message;
