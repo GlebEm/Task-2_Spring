e3f588f6714df1f3477a720db7ffb4f8b7f9716f
GlebEm <gleb.emelyanov.2013@mail.ru>
2023-07-10 13:08:55 +0300

    Spring_FirstTask

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean.equals(bean1));
+
+        ApplicationContext applicationContext1 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat bean2 =
+                (Cat) applicationContext1.getBean("Cat");
+
+        Cat bean3 =
+                (Cat) applicationContext1.getBean("Cat");
+        System.out.println(bean2.equals(bean3));
     }
 }
